41172d3182049524e162d8cb9b08d1ebe5df7f50
Nediljko Andrijanic <nedi2117@gmail.com>
2026-06-16 18:25:41 +0200

    Rebuild with correct base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   // IMPORTANT: Replace <REPO_NAME> with your GitHub repository name.
   // If you use a custom domain or a username.github.io repo, set base: '/'.
-  base: '/<REPO_NAME>/',
-})
+  base: "/vocabulary-app/",
+});
